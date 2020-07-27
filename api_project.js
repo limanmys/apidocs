a506e39ced13e5b4be84b3572abaae67f3eeb8ea
limanmys <mcelen94@gmail.com>
2020-07-27 14:19:29 +0000

    Update from https://github.com/mertcelen/liman/commit/b76fff8c8fa9a54ddd0a88b48db6689fec86e241)

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-07-27T05:20:24.793Z",
+    "time": "2020-07-27T14:19:24.232Z",
     "url": "http://apidocjs.com",
     "version": "0.23.0"
   }
